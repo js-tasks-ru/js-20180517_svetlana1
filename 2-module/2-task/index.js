02c9b1c616518d7c0ee19f224ccb441f8b6a1384
s_zolotorevich <s_zolotorevich@wargaming.net>
2018-06-18 19:29:55 +0300

    2 module 2 task: Функция find

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,5 +4,32 @@
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
+function find(obj, value) {
+    let result = [];
+
+    function findPath(object, root) {
+        for (let key in object) {
+            let path = root !== undefined ? root + '.' + key : key;
+            let val = object[key];
+            if (val === value) {
+                result.push(path);
+            }
+            else {
+                if (typeof val === 'object') {
+                    findPath(val, path);
+                }
+            }
+        }
+    }
+
+    findPath(obj);
+
+    switch (result.length) {
+        case 0:
+            return null;
+        case 1:
+            return result[0];
+        default:
+            return result;
+    }
 }
