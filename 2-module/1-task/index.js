3b973fcb8241539d3265528ea271b25380a1d022
s_zolotorevich <s_zolotorevich@wargaming.net>
2018-07-01 18:23:25 +0300

    9 module 1 task: extendClass 2 module 1 task: clone

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,13 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-function clone (obj) {
+function clone(obj) {
+    if (typeof obj !== 'object' || !obj) {
+        return obj
+    }
+    let result = {};
+    for (let key in obj) {
+        result[key] = clone(obj[key]);
+    }
+    return result;
 }
